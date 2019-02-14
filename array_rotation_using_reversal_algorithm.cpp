c8c6b6ff6428e8d5663fb594181f26e9571b01fc
Kaushal Sharma <kaushalsharma880@gmail.com>
2019-02-14 18:39:41 +0530

    rotation using reversal algorithm

--- array_rotation_using_reversal_algorithm.cpp
+++ array_rotation_using_reversal_algorithm.cpp
@@ -3,11 +3,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void reversearray(int arr[], int start, int end);
 void reversal_algorithm(int arr[], int size, int d);
-void reverse(int arr[], int start, int end);
 
 int main()
 {
+
 #ifndef LOCAL_TEST
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
@@ -28,25 +29,25 @@ int main()
 	return 0;
 }
 
-void reversal_algorithm(int arr[], int size, int d)
-{
-	reverse(arr, 0, d);
-	reverse(arr, d, size);
-	reverse(arr, 0, size);
-
-	for (int i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-void reverse(int arr[], int start, int end)
+void reversearray(int arr[], int start, int end)
 {
 	while (start < end)
 	{
 		int temp = arr[start];
-		arr[start] = arr[end - 1];
-		arr[end - 1] = temp;
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
 	}
-	start++;
-	end--;
+}
+
+void reversal_algorithm(int arr[], int size, int d)
+{
+	reversearray(arr, 0, d - 1);
+	reversearray(arr, d, size - 1);
+	reversearray(arr, 0, size - 1);
+
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 }
