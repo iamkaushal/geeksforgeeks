4054ebc189a1854bb280038ca401e24650d392ef
Kaushal Sharma <kaushalsharma880@gmail.com>
2019-02-14 17:27:15 +0530

    array rotation using temp array

--- array_rotation.cpp
+++ array_rotation.cpp
@@ -1,14 +1,47 @@
+// array rotation using temp array
+
 #include<bits/stdc++.h>
 using namespace std;
 
+void leftArrayRotate(int arr[], int n, int d);
+
 int main()
 {
-	#ifndef LOCAL_TEST
+
+#ifndef LOCAL_TEST
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	#endif
+#endif
 
-	cout << "hello" << endl;
+	int n ;
+	cin >> n;
+
+	int d; 
+	cin >> d;
+
+	int arr[n];
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	leftArrayRotate(arr, n, d);
 
 	return 0;
+}
+
+void leftArrayRotate(int arr[], int n , int d)
+{
+	int temp[d];
+	for (int i = 0; i < d; i++)
+		temp[i] = arr[i];
+
+	for (int i = 0; i < n - d ; i++)
+		arr[i] = arr[i + d];
+
+	for (int i = n - d, j = 0; i < n; i++, j++)
+		arr[i] = temp[j];
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+
+	cout << endl;
 }
