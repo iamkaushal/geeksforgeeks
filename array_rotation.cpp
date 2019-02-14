c44eaaa6d5ec5824de21d216f3098df69baa6de1
Kaushal Sharma <kaushalsharma880@gmail.com>
2019-02-14 17:06:56 +0530

    commit testing

---
+++ array_rotation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	#ifndef LOCAL_TEST
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	cout << "hello" << endl;
+
+	return 0;
+}
